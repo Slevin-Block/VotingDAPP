ae7844b5082df23e4fe865f0508e451b46bb4427
Slevin-Block <slevin@SlevinBlock>
2023-03-07 09:42:53 +0100

    [ci] Cleaning and test link with Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,9 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
+        Hello
     </EthProvider>
   );
 }
