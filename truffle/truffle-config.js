c8c820abe7fc6d0786882ff71698b982ada77d12
tuxin <dauge.damien@gmail.com>
2023-03-08 16:11:28 +0100

    Contract modification

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -44,7 +44,8 @@
 // require('dotenv').config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+require('dotenv').config();
 
 module.exports = {
   /**
@@ -65,11 +66,29 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+     development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+     goerli:{
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: `${process.env.MNEMONIC}`
+          },
+          providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`}),
+          network_id: 5
+    },
+    mumbai:{
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: `${process.env.MNEMONIC}`
+          },
+          providerOrUrl: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`}),
+          network_id: 80001
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -107,7 +126,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.18",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
