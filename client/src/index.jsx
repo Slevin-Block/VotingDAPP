299b1ab7de281249f01000340e46f09975b1ca90
Slevin-Block <slevin@SlevinBlock>
2023-03-10 11:36:53 +0100

    [refactor] EthProvider modifications

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ import { theme } from './assets/theme/theme';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RecoilRoot>
+        <RecoilRoot>``
             <EthProvider>
                 <ChakraProvider theme={theme}>
                     <App />
