5c714feb1ad6c80779df48f6dce9149ecd32c34f
tuxin <dauge.damien@gmail.com>
2023-03-18 09:59:20 +0100

    Deploy to mumbai
    Deploy to mumbai

--- client/src/contexts/RxWeb3.js
+++ client/src/contexts/RxWeb3.js
@@ -4,6 +4,7 @@ import Web3 from 'web3';
 
 const JSON_File = "Voting.json"
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+web3.currentProvider.setMaxListeners(300)
 const schemaReq = { account: null, networkID: null, artifact: null, contractAddress: null, contract: null, blockNumber: null }
 
 
